012c90bb74247a874ff7a1b99a116020513177cc
Bogdan-Ost <bogdan.ostapenko.97@gmail.com>
2025-10-08 18:06:46 +0000

    Deploying to gh-pages from @ Bogdan-Ost/goit-js-hw-10@0fcca47f27ed7ac4f406913d32c1c58b447d451e 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DgiiQB6G.js";import{f as e}from"./assets/vendor-EyZmBGcZ.js";const t=document.querySelector("#datetime-picker"),n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(o){}};e(t,n);
+//# sourceMappingURL=1-timer.js.map
