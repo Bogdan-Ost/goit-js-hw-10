0fcca47f27ed7ac4f406913d32c1c58b447d451e
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2025-10-08 21:06:16 +0300

    begin

---
+++ src/js/1-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates;
+  },
+};
+
+let userSelectedDate = flatpickr(input, options);
