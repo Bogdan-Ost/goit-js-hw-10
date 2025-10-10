fed5730eeaaaea5eefeb0c7ccecbb857c4a66414
Bogdan-Ost <bogdan.ostapenko.97@gmail.com>
2025-10-10 14:03:10 +0000

    Deploying to gh-pages from @ Bogdan-Ost/goit-js-hw-10@607287ae07bc5fdb6c213f6324c92fe0efcaafdf 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DgiiQB6G.js";const o=document.querySelector(".form");o.addEventListener("submit",t);function t(e){console.log(e.target)}
+//# sourceMappingURL=2-snackbar.js.map
